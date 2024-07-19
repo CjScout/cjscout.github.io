11e3911355290839506082a6422396790ec747d1
Connor Schuler <cschuler@iol.unh.edu>
2024-07-19 14:14:56 -0400

    Project rebuild with --no-standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
