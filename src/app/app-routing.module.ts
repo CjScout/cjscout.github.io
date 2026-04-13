d1e9d7c80805041ea295c2a75bc5f323c4fffaa4
Connor Schuler <cjsbeverly@gmail.com>
2026-04-13 00:25:46 -0400

    Refactor project structure, update styles, and enhance blog and home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { BlogComponent } from './blog/blog.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'blog', component: BlogComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: '**', component: ErrorPageComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
-import { BlogComponent } from './blog/blog.component';
-import { AboutComponent } from './about/about.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 // Modules
-import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
-import { StepperModule } from 'primeng/stepper';
 import { TooltipModule } from 'primeng/tooltip';
 import { DividerModule } from 'primeng/divider';
 import { CardModule } from 'primeng/card';
@@ -25,18 +21,14 @@ import { CardModule } from 'primeng/card';
     AppComponent,
     MenuComponent,
     HomeComponent,
-    BlogComponent,
-    AboutComponent,
     ErrorPageComponent,
     ProjectsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AccordionModule,
     ButtonModule,
     BrowserAnimationsModule,
-    StepperModule,
     TooltipModule,
     DividerModule,
     CardModule
